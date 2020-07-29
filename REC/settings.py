57ad8a35a4603b4e13fa3a26024f40c524e7d558
Nishi-Mahato <54651764+Nishi-Mahato@users.noreply.github.com>
2020-07-29 18:02:52 -0500

    Update settings.py

--- REC/settings.py
+++ REC/settings.py
@@ -131,10 +131,5 @@ LOGOUT_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'nishimahto@gmail.com'
-EMAIL_HOST_PASSWORD = 'fhyrxccdliqldtsx'
+
 
